bbc8a285928c482d7d5ea70589b50a938fff324c
Maria Plocek <255676@edu.p.lodz.pl>
2026-05-17 21:11:21 +0200

    dodanie przyciskow start stop oraz zapisz

--- src/menu/Trening_Wspierany.py
+++ src/menu/Trening_Wspierany.py
@@ -1,9 +1,10 @@
 import detection.excersises as ex
+from kivy.core.image import Texture
 from kivy.uix.label import Label
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.uix.popup import Popup
-
+import cv2
 from TwoCameraFrameWindow import TwoCameraFrameWindow as TCFW
 from RoundedButton import *
 
@@ -11,6 +12,181 @@ class TreningWspierany(TCFW):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.screenExcersise = ex.LowReady()
+
+        self.is_training_started = False # Czy trening już się zaczął
+        self.hover_start_frames = 0 # licznik "czasu" jaki musi minąć aby uruchomić przycisk start/stop
+        self.hover_save_frames = 0 # licznik "czasu" dla przycisku zapisz
+        self.HOVER_THRESHOLD = 30 # ile czasu potrzba aby aktywować przyciski
+        self.has_training_run = False  # Czy było start i potem stop (trening się zaczął i skończyć -> można go zapisać)
+        self.is_training_saved = False  # Czy trening został już zapisany
+
+    def update_frame(self, dt):
+        if not self.cap or not self.cap.isOpened() and not self.cap2 or not self.cap2.isOpened():
+            return
+
+        landmarksFront = None
+        landmarksSide = None
+
+        if self.cap.isOpened():
+            self.camera_view.texture, landmarksFront = self.update_camera(self.cap)
+
+        if self.cap2.isOpened():
+            self.camera_view2.texture, landmarksSide = self.update_camera(self.cap2, True)
+
+        if self.is_training_started:
+            if self.screenExcersise.checkExcersise(landmarksFront, landmarksSide):
+                pass  #TODO w jaki sposob sprawdzamy ćwiczenie????
+            msg = self.screenExcersise.getMessage()
+            self.text_box.text = msg
+        else:
+            self.text_box.text = "Czekam na start..."
+
+    def update_camera(self, cap, isSide: bool = False):
+        ret, frame = cap.read()
+        if ret:
+            frame = cv2.flip(frame, 1)
+
+            processed_frame, result = self.detector.process_frame(frame)
+            landmarks = self.detector.getLandmarks()
+
+            if not isSide:
+                processed_frame = self.handle_cv_interface(processed_frame)
+
+            buf = cv2.flip(processed_frame, 0).tobytes()
+            texture = Texture.create(
+                size=(processed_frame.shape[1], processed_frame.shape[0]),
+                colorfmt='bgr'
+            )
+            texture.blit_buffer(buf, colorfmt='bgr', bufferfmt='ubyte')
+
+            return texture, landmarks
+
+        return None, None
+
+    def handle_cv_interface(self, frame):
+        h, w, _ = frame.shape
+
+        btn_w, btn_h = int(w * 0.20), int(h * 0.12)
+
+        # Przycisk start/stop
+        s_x1, s_y1 = int(w * 0.05), int(h * 0.15)
+        s_x2, s_y2 = s_x1 + btn_w, s_y1 + btn_h
+
+        # Przycisk zapisz
+        sv_x1 = w - btn_w - int(w * 0.05)
+        sv_y1 = s_y1
+        sv_x2, sv_y2 = sv_x1 + btn_w, sv_y1 + btn_h
+
+        #sprawdzenie reakcji przycisku zapisz
+        can_save = self.has_training_run and not self.is_training_started and not self.is_training_saved
+
+        wrists = []
+        for idx in [15, 16]:
+            lm = self.detector.getLandmarkCords(idx)
+            if lm:
+                wrists.append((int(lm[0] * w), int(lm[1] * h)))
+
+        start_hovered = False
+        save_hovered = False
+
+        for wx, wy in wrists:
+            # Kolizja ze Start/Stop
+            if s_x1 <= wx <= s_x2 and s_y1 <= wy <= s_y2:
+                start_hovered = True
+            # Kolizja z Zapisz (aktywna tylko, gdy spełnione są warunki zapisu)
+            if sv_x1 <= wx <= sv_x2 and sv_y1 <= wy <= sv_y2:
+                if can_save:
+                    save_hovered = True
+
+        # Logika hover dla Start/Stop
+        if start_hovered:
+            if self.hover_start_frames >= 0:
+                self.hover_start_frames += 1
+                if self.hover_start_frames >= self.HOVER_THRESHOLD:
+                    self.toggle_start_stop()
+                    self.hover_start_frames = -30
+        else:
+            self.hover_start_frames = max(0, self.hover_start_frames - 2) \
+                if self.hover_start_frames > 0 else self.hover_start_frames
+
+        if self.hover_start_frames < 0:
+            self.hover_start_frames += 1
+
+        # Logika hover dla Zapisz
+        if save_hovered:
+            if self.hover_save_frames >= 0:
+                self.hover_save_frames += 1
+                if self.hover_save_frames >= self.HOVER_THRESHOLD:
+                    self.save_training()
+                    self.hover_save_frames = -30
+        else:
+            self.hover_save_frames = max(0,
+                                         self.hover_save_frames - 2) if self.hover_save_frames > 0 else self.hover_save_frames
+
+        if self.hover_save_frames < 0:
+            self.hover_save_frames += 1
+
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        font_scale = w / 1200.0
+        thickness = 1
+        # przycisk start-stop
+        bg_start = (0, 0, 200) if self.is_training_started else (0, 180, 0)
+        cv2.rectangle(frame, (s_x1, s_y1), (s_x2, s_y2), bg_start, -1)
+
+        if self.hover_start_frames > 0:
+            fill_w = int((self.hover_start_frames / self.HOVER_THRESHOLD) * btn_w)
+            cv2.rectangle(frame, (s_x1, s_y1), (s_x1 + fill_w, s_y2), (0, 255, 255), -1)
+
+        text_start = "STOP" if self.is_training_started else "START"
+
+        (t_w, t_h), _ = cv2.getTextSize(text_start, font, font_scale, thickness)
+        t_x = s_x1 + (btn_w - t_w) // 2
+        t_y = s_y1 + (btn_h + t_h) // 2
+        cv2.putText(frame, text_start, (t_x, t_y), font, font_scale, (255, 255, 255), thickness)
+
+        # Przycisk zapisz
+        if self.is_training_saved:
+            bg_save = (150, 0, 0)  # ciemno niebieski - potwierdzenie zapisu
+            text_save = "ZAPISANO"
+        elif can_save:
+            bg_save = (200, 100, 0)  # Niebieski -
+            text_save = "ZAPISZ"
+        else:
+            bg_save = (100, 100, 100)  # Szary - zablokowany
+            text_save = "ZAPISZ"
+
+        cv2.rectangle(frame, (sv_x1, sv_y1), (sv_x2, sv_y2), bg_save, -1)
+
+        if self.hover_save_frames > 0 and can_save:
+            fill_w = int((self.hover_save_frames / self.HOVER_THRESHOLD) * btn_w)
+            cv2.rectangle(frame, (sv_x1, sv_y1), (sv_x1 + fill_w, sv_y2), (0, 255, 255), -1)
+
+        (t_w, t_h), _ = cv2.getTextSize(text_save, font, font_scale, thickness)
+        t_x = sv_x1 + (btn_w - t_w) // 2
+        t_y = sv_y1 + (btn_h + t_h) // 2
+        cv2.putText(frame, text_save, (t_x, t_y), font, font_scale, (255, 255, 255), thickness)
+
+        return frame
+
+    def toggle_start_stop(self):
+        self.is_training_started = not self.is_training_started
+
+        if self.is_training_started:
+            print("Trening ROZPOCZĘTY")
+            # Kiedy startujemy nowy trening, resetujemy flagi zapisu
+            self.has_training_run = False
+            self.is_training_saved = False
+        else:
+            print("Trening ZATRZYMANY")
+            # Kiedy zatrzymujemy, ustawiamy, że trening się odbył i można go zapisać
+            self.has_training_run = True
+
+        # TODO logika do startu stopu przycisku
+
+    def save_training(self):
+        print("Trening ZAPISANY!")
+        self.is_training_saved = True  # Zablokowanie przycisku
+        # TODO logika zapisu treningu
 
     def change_screen(self, target_screen, instance):
         if target_screen == 'menu':
