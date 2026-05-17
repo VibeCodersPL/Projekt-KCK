750b133535a2e79309f7cc972785593d9db5350c
Maria Plocek <255676@edu.p.lodz.pl>
2026-05-17 20:19:11 +0200

    dodanie komunikatu przy probie powrotu do menu

--- src/menu/Trening_Wspierany.py
+++ src/menu/Trening_Wspierany.py
@@ -1,7 +1,67 @@
 import detection.excersises as ex
+from kivy.uix.label import Label
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.uix.popup import Popup
+
 from TwoCameraFrameWindow import TwoCameraFrameWindow as TCFW
+from RoundedButton import *
 
 class TreningWspierany(TCFW):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.screenExcersise = ex.LowReady()
+
+    def change_screen(self, target_screen, instance):
+        if target_screen == 'menu':
+            self.show_exit_confirmation(target_screen)
+        else:
+            super().change_screen(target_screen, instance)
+
+    def show_exit_confirmation(self, target_screen):
+        content_layout = BoxLayout(orientation='vertical', spacing=20, padding=20)
+
+        msg_label = Label(
+            text="Czy chcesz przerwać trening i wyjść?",
+            size_hint_y=0.7,
+            halign='center',
+            valign='middle',
+            font_size = '24sp'
+        )
+        msg_label.bind(size=msg_label.setter('text_size'))
+
+        buttons_layout = BoxLayout(orientation='horizontal', spacing=20, size_hint_y=0.3)
+
+        btn_yes = RoundedButton(text="Tak",
+                    font_size='26sp',
+                    color=(1, 1, 1, 1),
+                    bg_color=(0.15, 0.45, 0.85, 1),
+                    radius=45)
+        btn_no = RoundedButton(text="Nie",
+                    font_size='26sp',
+                    color=(1, 1, 1, 1),
+                    bg_color=(0.15, 0.45, 0.85, 1),
+                    radius=45)
+
+        buttons_layout.add_widget(btn_yes)
+        buttons_layout.add_widget(btn_no)
+
+        content_layout.add_widget(msg_label)
+        content_layout.add_widget(buttons_layout)
+
+        self.exit_popup = Popup(
+            title='Przerwanie treningu',
+            content=content_layout,
+            size_hint=(0.4, 0.4),
+            auto_dismiss=False
+        )
+
+        btn_yes.bind(on_press=lambda inst: self.confirm_exit(target_screen))
+        btn_no.bind(on_press=self.exit_popup.dismiss)
+
+        self.exit_popup.open()
+
+    def confirm_exit(self, target_screen):
+        self.exit_popup.dismiss()
+        print("Trening przerwany. Ćwiczenie nie zostało zapisane.")
+        self.manager.current = target_screen
