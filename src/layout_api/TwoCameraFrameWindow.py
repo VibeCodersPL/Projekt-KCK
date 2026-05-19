d962d3bd6243126f43aa539a1b8c9332cc1edb32
PatrykOlszewski1 <255668@edu.p.lodz.pl>
2026-05-20 00:04:16 +0200

    Poprawa konstruktora klasy TCFW

--- src/layout_api/TwoCameraFrameWindow.py
+++ src/layout_api/TwoCameraFrameWindow.py
@@ -1,5 +1,6 @@
 from kivy.properties import partial
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.screenmanager import Screen
 from kivy.uix.button import Button
 from kivy.uix.label import Label
@@ -11,21 +12,27 @@ import detection.excersises as Ex
 
 
 class TwoCameraFrameWindow(Screen):
-
     screenExcersise: Ex.Exercise = None
 
     def __init__(self, **kwargs):
-
         super().__init__(**kwargs)
         self.landmarksFront = None
-        self.landmarksFront = None
-        #TODO to jest do poukładania
-        btn = Button(text="Powrot do Menu",size_hint=(None,None), size=(200, 50))
-        btn.bind(on_press=partial(self.change_screen, 'menu'))
-        cameras_layout = BoxLayout(orientation = "horizontal", spacing=15, size_hint=(1,0.8))
+        self.landmarksSide = None
 
-        self.camera_view = Image(size_hint=(1, 1), pos_hint={'center_x': 0.25, 'center_y': 0.5}, allow_stretch = True)
-        self.camera_view2 = Image(size_hint=(1, 1), pos_hint={'center_x': 0.25, 'center_y': 0.5},  allow_stretch = True)
+        self.main_layout = FloatLayout()
+
+        self.cameras_layout = BoxLayout(orientation="horizontal", spacing=15, size_hint=(1, 0.8),
+                                        pos_hint={'center_y': 0.45})
+        self.camera_view = Image(size_hint=(1, 1), allow_stretch=True)
+        self.camera_view2 = Image(size_hint=(1, 1), allow_stretch=True)
+        self.cameras_layout.add_widget(self.camera_view)
+        self.cameras_layout.add_widget(self.camera_view2)
+
+        self.ui_layer = FloatLayout()
+
+        btn = Button(text="Powrot do Menu", size_hint=(None, None), size=(200, 50))
+        btn.bind(on_press=partial(self.change_screen, 'menu'))
+
         self.text_box = Label(
             size_hint=(0.6, 0.1),
             pos_hint={'center_x': 0.5, 'top': 0.98},
@@ -35,31 +42,20 @@ class TwoCameraFrameWindow(Screen):
             valign='middle'
         )
         self.text_box.bind(size=self.text_box.setter('text_size'))
-        cameras_layout.add_widget(self.camera_view)
-        cameras_layout.add_widget(self.camera_view2)
 
-        self.add_widget(btn)
-        self.add_widget(cameras_layout)
-        self.add_widget(self.text_box)
+        self.ui_layer.add_widget(btn)
+        self.ui_layer.add_widget(self.text_box)
 
-    def add_ui_element(self, widget):
-        self.ui_layer.add_widget(widget)
-
-    def remove_ui_element(self, widget):
-        self.ui_layer.remove_widget(widget)
-
-    def set_title_text(self, text, color=(1, 1, 1, 1)):
-        self.text_box.text = text
-        self.text_box.color = color
-
+        self.main_layout.add_widget(self.cameras_layout)
+        self.main_layout.add_widget(self.ui_layer)
+        self.add_widget(self.main_layout)
 
     def change_screen(self, target_screen, instance):
         self.manager.current = target_screen
-        
+
     def on_enter(self):
         self.detector = self.manager.shared_detector
         Clock.schedule_once(self._late_camera_init, 0.2)
-
 
     def _late_camera_init(self, dt):
         self.cap = cv2.VideoCapture(0)
@@ -73,7 +69,6 @@ class TwoCameraFrameWindow(Screen):
         else:
             print("Kamera nadal zablokowana przez system.")
 
-            
     def update_frame(self, dt):
             if not self.cap or not self.cap.isOpened() and not self.cap2 or not self.cap2.isOpened():
                 return
@@ -83,8 +78,7 @@ class TwoCameraFrameWindow(Screen):
                 
             if(self.cap2.isOpened()):
                 self.camera_view2.texture, self.landmarksSide = self.update_camera(self.cap2, True)
-            
-                
+
     def update_camera(self, cap, isSide:bool = False):
         ret, frame = cap.read()
         if ret:
