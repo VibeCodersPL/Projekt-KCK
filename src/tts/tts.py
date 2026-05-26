d8f2840c24562c775b68f59c6db72c5b8c228cf7
mateuszgrabinski <255592@edu.p.lodz.pl>
2026-05-26 10:59:51 +0200

    Dodanie klasy TTS

---
+++ src/tts/tts.py
@@ -0,0 +1,23 @@
+import pyttsx3
+
+
+class TTS:
+    def __init__(self):
+        self.engine = pyttsx3.init()
+        self.engine.setProperty("rate", 125)
+        self.engine.setProperty("volume", 1.0)
+        self.set_polish_voice()
+
+    def set_polish_voice(self):
+        voices = self.engine.getProperty("voices")
+        for voice in voices:
+            voice_name = voice.name.lower()
+            voice_id = voice.id.lower()
+            if "polish" in voice_name or "pl" in voice_id:
+                self.engine.setProperty("voice", voice.id)
+                return
+        print("Nie znaleziono polskiego głosu")
+
+    def speak(self, phrase):
+        self.engine.say(phrase)
+        self.engine.runAndWait()
